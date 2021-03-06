b140a05f2bd03028920e6b655012763c0ac52e4f
David Mallasen <davidmq@kth.se>
2021-03-06 10:57:12 +0100

    Add more tests and build instructions

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# gemm_adv
-Advanced general matrix multiplication
+# Advanced general matrix multiplication (gemm) 
+
+First clone this repo by running:
+
+~~~
+git clone https://github.com/davidmallasen/gemm_adv.git
+cd gemm_adv
+~~~
+
+## Unit testing
+
+In the root of the repo run:
+
+~~~
+./autogen.sh
+./configure
+make
+make check
+~~~
+
+To run get the full output run `./tests/func_test`.
--- tests/test.cpp
+++ tests/test.cpp
@@ -5,14 +5,41 @@ extern "C" {
 #include "gemm.h"
 }
 
+// Basic simple test with 1x1 matrices
 TEST(GemmTest, SimpleGemm) {
-    int eye[] = {1, 0, 0, 1};
-    int incr[] = {1, 2, 3, 4};
+    int a[] = {1};
+    int b[] = {2};
+    int gemm_sol[] = {0};
+    
+    int sol[] = {2};
+    
+    gemm(1, gemm_sol, a, b);
+
+    EXPECT_THAT(gemm_sol, testing::ContainerEq(sol));
+}
+
+// Test major ordering with non-symmetric 2x2 matrices
+TEST(GemmTest, RowMajor) {
+    int a[] = {1, 2, 3, 4};
+    int b[] = {5, 6, 7, 8};
     int gemm_sol[] = {0, 0, 0, 0};
     
-    int sol[] = {1, 2, 3, 4};
+    int sol[] = {19, 22, 43, 50};
     
-    gemm(2, gemm_sol, incr, eye);
+    gemm(2, gemm_sol, a, b);
+
+    EXPECT_THAT(gemm_sol, testing::ContainerEq(sol));
+}
+
+// Test higher dimension matrices and negative numbers
+TEST(GemmTest, HigherDimNeg) {
+    int a[] = {1, 2, 3, 4, 5, 6, 7, 8, -1, -2, -3, -4, -5, -6, -7, -8};
+    int b[] = {8, 6, 7, 5, -4, -2, -3, -1, -8, -6, -7, -5, 4, 2, 3, 1};
+    int gemm_sol[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    
+    int sol[] = {-8, -8, -8, -8, -8, -8, -8, -8, 8, 8, 8, 8, 8, 8, 8, 8};
+    
+    gemm(4, gemm_sol, a, b);
 
     EXPECT_THAT(gemm_sol, testing::ContainerEq(sol));
 }
